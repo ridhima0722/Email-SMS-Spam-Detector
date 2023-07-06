4099c2f1f6da36a2093a5926a5ee1c1b8f4410d3
Ridhima <89964302+ridhima0722@users.noreply.github.com>
2023-07-06 11:50:51 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -30,27 +30,25 @@ def transform_text(text):
         
     return " ".join(y)    
 
-
-
-
-tfidf =pickle.load(open('vectorizer.pkl','rb'))
-model=pickle.load(open('model.pkl','rb'))
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
 
 
 st.title("Email/SMS Spam Classifier")
 
 input_sms=st.text_area("Enter the message")
-
-
 if st.button('Predict'):
-#1.preprocess 
-    transformed_sms=transform_text(input_sms)
-    #2.vectorize
-    vectorised_input=tfidf.transform([transformed_sms])
-    #3.predict
-    result=model.predict(vectorised_input)[0]
-    #4.Display
-    if result==1:
+    # 1. Preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. Vectorize
+    vectorised_input = tfidf.transform([transformed_sms])
+    # 3. Predict
+    result = model.predict(vectorised_input)[0]
+    print(result)
+    # 4. Display
+    if result == 1:
         st.header("Spam")
     else:
-        st.header("Not Spam")    
+        st.header("Not Spam")
+
+
